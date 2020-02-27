9da903a7787fafaea578b6bfffa92a05c393a920
Eng-Abdelrahman-M <abdelrahman.m.a@gmail.com>
2020-02-27 13:55:42 +0200

    fix files

---
+++ gighub/View_Models/ForgotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gighub.View_Models
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
